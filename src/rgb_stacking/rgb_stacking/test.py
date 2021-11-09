7f2342f68e8fbb6055e908ba11073f336375e04a
root <30609374+alfaevc@users.noreply.github.com>
2021-11-08 23:01:16 -0500

    get action

--- src/rgb_stacking/rgb_stacking/test.py
+++ src/rgb_stacking/rgb_stacking/test.py
@@ -2,8 +2,11 @@ from typing import Sequence
 
 from absl import app
 from dm_control import viewer
+from dm_robotics.moma import action_spaces
 
 from rgb_stacking import environment
+
+import numpy as np
 
 
 def main(argv: Sequence[str]) -> None:
@@ -12,8 +15,23 @@ def main(argv: Sequence[str]) -> None:
 
     # Load the rgb stacking environment.
     env = environment.rgb_stacking(observation_set=environment.ObservationSet.VISION_ONLY, object_triplet='rgb_test_triplet1')
-    state = env.reset()
-    print(state)
+    step_type, reward, discount, obs = env.reset()
+    print(step_type)
+    print(reward)
+    print(discount)
+    for i in obs:
+        print("The dimension of {0} is {1}.".format(i, obs[i].shape))
+    
+    state = np.concatenate((obs["basket_front_left/pixels"], obs["basket_front_right/pixels"]), axis=1)
+    action = np.array([7.00e-02,7.00e-02, 7.00e-02, 1.00e+00, 2.55e+02])/2
+    _, next_reward, _, next_obs = env.step(action)
+    for i in next_obs:
+        print("The dimension of {0} is {1}.".format(i, obs[i].shape))
+    
+
+    print(env.subtask._action_space._component_action_spaces)
+    print(env.subtask._action_space._composite_action_spec)
+
 
     # Launch the viewer application.
     # viewer.launch(env)
