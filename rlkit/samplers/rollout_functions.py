af6f08e2b8109c06dd4d84413ac69e43c67c243d
ztan <zt2291@columbia.edu>
2021-11-16 20:10:01 -0500

    what

--- rlkit/samplers/rollout_functions.py
+++ rlkit/samplers/rollout_functions.py
@@ -105,7 +105,8 @@ def rollout(
         reset_callback(env, agent, o)
     if render:
         env.render(**render_kwargs)
-    while path_length < max_path_length:
+    # while path_length < max_path_length:
+    while path_length < 400: # lets just hardcode ..
         raw_obs.append(o)
         o_for_agent = preprocess_obs_for_policy_fn(o)
         # print(o.shape, o_for_agent.shape)
@@ -117,10 +118,10 @@ def rollout(
         # next_o, r, done, env_info = env.step(copy.deepcopy(a))
         step_type, r, discount, next_obs = env.step(copy.deepcopy(a))
         next_o = np.concatenate((obs["basket_front_left/pixels"], obs["basket_front_right/pixels"]), axis=1)
-        done = True if np.isclose(discount, 1) else False
+        # done = True if np.isclose(discount, 1) else False
         # we have to set time limit stuffs, and determine if task is finished
-        if step_type not in ["StepType.MID", "StepType.FIRST"]:
-            print(step_type, type(step_type), path_length)
+        # if step_type not in ["StepType.MID", "StepType.FIRST"]:
+        #     print(step_type, type(step_type), path_length)
         if render:
             env.render(**render_kwargs)
         observations.append(o)
@@ -152,6 +153,8 @@ def rollout(
     rewards = np.array(rewards)
     if len(rewards.shape) == 1:
         rewards = rewards.reshape(-1, 1)
+
+    print(path_length)
     return dict(
         observations=observations,
         actions=actions,
