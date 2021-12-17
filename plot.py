7b24067731304d4d58d68ab5aea454f3e19bd477
Alvin Pan <qp2134@columbia.edu>
2021-12-16 22:17:41 -0500

    add plot.py

--- plot.py
+++ plot.py
@@ -10,25 +10,19 @@ def load_data(path):
     return header, data
 
 if __name__ == '__main__':
-    header, data = load_data('data/name-of-experiment/name-of-experiment_2021_11_19_14_23_55_0000--s-0/progress.csv')
-    ns = range(data.shape[0])
-    print(header)
-    print(header[10])
-    print(header[11])
-    print(header[12])
-    avs = data[:,11]
-    maxs = data[:,10]
-    mins = data[:,12]
-
+    header, data = load_data('slac_triplet1.csv')
+    
+    steps = data[:,0]
+    rs = data[:,1]
     # method = "mixed"
 
-    plt.fill_between(ns, mins, maxs, alpha=0.1)
-    plt.plot(ns, avs, '-o', markersize=1, label="Train")
+    # plt.fill_between(ns, mins, maxs, alpha=0.1)
+    plt.plot(steps, rs, '-o', markersize=1, label="Triplet 1")
 
     plt.legend()
     plt.grid(True)
     plt.xlabel('Time step', fontsize = 15)
     plt.ylabel('Return', fontsize = 15)
 
-    plt.title("SAC PCA", fontsize = 24)
-    plt.savefig("plots/SAC PCA.png")
+    plt.title("SLAC Learning Curve", fontsize = 24)
+    plt.savefig("plots/slac.png")
